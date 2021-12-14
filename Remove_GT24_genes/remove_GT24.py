dbf81d214fbbfc6464c018cc284e9379153e43f3
claireleblanc <67252968+claireleblanc@users.noreply.github.com>
2021-12-14 13:32:11 -0800

    Update remove_GT24.py

--- Remove_GT24_genes/remove_GT24.py
+++ Remove_GT24_genes/remove_GT24.py
@@ -1,4 +1,11 @@
-##Somehow read the command line arguments
+"""
+Author: Claire LeBlanc
+Last updated: 12/14/2021
+
+Overview: Removes genes with a more significant GT24 domain than GT8 to prevent
+misidentification of GT24 genes as GT8 genes. 
+"""
+
 import argparse, textwrap
 import sys
 
@@ -30,6 +37,8 @@ GT24Dict = {}
 
 GT24lines = GT24File.readlines()
 j = 0
+
+# Creates a dictionary containing each GT24 gene and the associated significance of the GT24 domain
 while (j < len(GT24lines)):
         line = GT24lines[j]
         if ("#" not in line): 
@@ -37,17 +46,19 @@ while (j < len(GT24lines)):
             GT24Dict[words[2]] = float(words[4])    
         j+=1
 
+# Uses the earlier dictionary to compare the significance of GT8 genes with the corresponding GT24 significance
+# If there is no GT24 domain, than the gene is automatically included
 GT8lines = GT8File.readlines()
 i = 0
 while (i < len(GT8lines)): 
     line = GT8lines[i]
     if ("#" not in line): 
         words = line.split()
-        try: 
+        if GT24Dict.has_key(words[2]): 
             Eval = GT24Dict[words[2]]
             test = Eval > float(words[4])
             if(test): 
                 print(words[2])
-        except: 
+        else:
             print(words[2])
     i+=1
